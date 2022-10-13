dae859fe4a459fcd19ed4bf8011f0f7d296677fe
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2022-10-14 00:39:01 +0700

    rem flush

--- warp.py
+++ warp.py
@@ -77,5 +77,4 @@ while True:
   sys.stdout.write(f"Cooldown: {cooldown} seconds" + "\n")
   time.sleep(cooldown)
 
-  sys.stdout.write(f"End: ------------------" + "\n")
-  sys.stdout.flush()
+  sys.stdout.write(f"End: ------------------" + "\n")]
